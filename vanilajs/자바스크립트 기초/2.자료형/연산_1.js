79dc9bd64d9c370d0b9c8b61abed0de96aaef98c
kwonkijun <kkj6369@naver.com>
2020-09-22 00:02:56 +0900

    a

---
+++ vanilajs/자바스크립트 기초/2.자료형/연산_1.js
@@ -0,0 +1,6 @@
+// 출력값을 예상해 보세요.
+
+console.log(11 + 3 * 4);
+console.log(25 / 5 + 15 % 4);
+console.log(33 - 5 ** 2);
+console.log((3 + 27) * 12 ** (5 % 3));
---
+++ vanilajs/자바스크립트 기초/5.함수/index_2.js
@@ -0,0 +1,7 @@
+// 여기에 코드를 입력해 주세요.
+
+
+// TB -> GB 테스트
+teraToGiga(2);
+// TB -> MB 테스트
+teraToMega(2);
