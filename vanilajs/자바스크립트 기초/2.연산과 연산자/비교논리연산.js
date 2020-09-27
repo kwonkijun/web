ab65bc06d454dcb3e13e9979151cc658e128574f
kwonkijun <kkj6369@naver.com>
2020-09-27 13:11:55 +0900

    a

---
+++ vanilajs/자바스크립트 기초/2.연산과 연산자/비교논리연산.js
@@ -0,0 +1,14 @@
+// 각 문장의 결과값을 예측해 보세요.
+
+// 1. AND 연산
+console.log(2 < 1 && 'Hello' !== 'Hello');
+
+
+// 2. OR 연산
+console.log(7 !== 7 || 4 < 3);
+
+
+// 3. 변수와 결합
+let x = 3;
+console.log(x > 4 || !(x > 2));
+
